c05c00434527331f9516a963c14f274553fa067a
Tomasz Maciążek <tommac.opole@gmail.com>
2022-12-02 12:07:37 +0100

    Initial Api structure

---
+++ src/PiecykPolHurt/PiecykPolHurt.DataLayer/DependencyInjection.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace PiecykPolHurt.DataLayer
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddDataLayer(this IServiceCollection services, IConfiguration configuration)
+        {
+            return services;
+        }
+    }
+}
