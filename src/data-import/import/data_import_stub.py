9a3af0e62a23fb2911e37ce22ac5a1ba456a7a7f
lukasz <lukasz.kocur.praca@gmail.com>
2023-02-01 23:20:00 +0100

    data import with exemplary proper import file

--- src/data-import/import/data_import_stub.py
+++ src/data-import/import/data_import_stub.py
@@ -1,18 +1,13 @@
 import grpc
-import greet_pb2
 import greet_pb2_grpc
 
-def run():
+def send(message):
     with grpc.insecure_channel('localhost:5203') as channel:     
-        stub = greet_pb2_grpc.ProductUpdaterStub(channel)    
-        named_request = greet_pb2.ProductUpdateRequest()
-        named_request.type.productType = greet_pb2.ProductType.GROSZEK
-        named_request.quantity = 1230
-        named_request.branch = "Ciechocinek"
-        named_reply = stub.UpdateProduct(named_request)
+        stub = greet_pb2_grpc.ProductUpdaterStub(channel)
+        named_reply = stub.UpdateProduct(message)
         print("Response received:")
         print(named_reply)
 
 
 if __name__== "__main__":
-    run()
+    send()
