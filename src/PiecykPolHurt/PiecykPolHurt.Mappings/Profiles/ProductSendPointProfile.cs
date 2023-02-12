9304d9276c5d39c543fc6de77cb5b1f77c9a31a8
Tomasz Maciążek <tommac.opole@gmail.com>
2023-02-12 20:47:44 +0100

    Added endpoint for today sendpoint products

--- src/PiecykPolHurt/PiecykPolHurt.Mappings/Profiles/ProductSendPointProfile.cs
+++ src/PiecykPolHurt/PiecykPolHurt.Mappings/Profiles/ProductSendPointProfile.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using PiecykPolHurt.Model.Commands;
+using PiecykPolHurt.Model.Dto.Product;
 using PiecykPolHurt.Model.Entities;
 
 namespace PiecykPolHurt.Mappings.Profiles;
@@ -18,5 +19,15 @@ public class ProductSendPointProfile : Profile
 
         CreateMap<ProductSendPoint, ProductSendPoint>()
             .ForMember(dest => dest.ForDate, opt => opt.Ignore());
+
+        CreateMap<ProductSendPoint, ProductSendPointListItemDto>()
+            .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.ProductId))
+            .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Product.Name))
+            .ForMember(dest => dest.Price, opt => opt.MapFrom(src => src.Product.Price))
+            .ForMember(dest => dest.Code, opt => opt.MapFrom(src => src.Product.Code))
+            .ForMember(dest => dest.Description, opt => opt.MapFrom(src => src.Product.Description))
+            .ForMember(dest => dest.ImageUrl, opt => opt.MapFrom(src => src.Product.ImageUrl))
+            .ForMember(dest => dest.IsActive, opt => opt.MapFrom(src => src.Product.IsActive))
+            .ForMember(dest => dest.AvailableQuantity, opt => opt.MapFrom(src => src.AvailableQuantity));
     }
 }
